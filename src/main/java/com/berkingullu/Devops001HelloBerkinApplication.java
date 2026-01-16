4ece4c5e066e396178fcb6bfb8e54e3efb3bbb4f
parapalazula99 <138779257+parapalazula99@users.noreply.github.com>
2026-01-16 20:05:01 +0300

    DevOps hello

---
+++ src/main/java/com/berkingullu/Devops001HelloBerkinApplication.java
@@ -0,0 +1,13 @@
+package com.berkingullu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops001HelloBerkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops001HelloBerkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkingullu/Devops001HelloBerkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkingullu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops001HelloBerkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
